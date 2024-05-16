56d8a57d37b4be4f4ee592e7f9007a5bd732df03
hojiakbarandaqulov <hojiakbarandaqulov5@gmail.com>
2024-05-16 20:20:46 +0500

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    public Employee(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,5 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+}
