c3f4ad69543e17f481df6d93cfc4bc6b7b1c3b53
hojiakbarandaqulov <hojiakbarandaqulov5@gmail.com>
2024-05-16 21:20:50 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/CarService.java

--- src/CarService.java
+++ src/CarService.java
@@ -1,4 +1,14 @@
 public class CarService {
 
     // Java Backend and Python Backend
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
